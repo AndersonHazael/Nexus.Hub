f3c02a5a1810de2d481bd9ba4796a4558b033d2e
AndersonHazael <117550667+AndersonHazael@users.noreply.github.com>
2024-03-03 00:19:02 -0300

    Update favicon.js

--- js/utils/favicon.js
+++ js/utils/favicon.js
@@ -2,10 +2,10 @@ const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matche
 const favicon = document.querySelector("link[rel='icon']") || document.createElement('link');
 
 if (prefersDarkMode){
-    favicon.href = '.img/favicon/favicon-light.png';
+    favicon.href = '../img/favicon/favicon-light.png';
 }
 else{
-    favicon.href = '.img/favicon/favicon-dark.png'
+    favicon.href = '../img/favicon/favicon-dark.png';
   
 }
 favicon.type = 'image/x-icon';
